71e2aa6c29103672607c35a71be1af82135d7be0
jennysival <jeniffersival@gmail.com>
2022-06-07 13:42:32 -0300

    Adicionar tab e viewPager

--- app/src/main/java/br/com/zup/minhaMusicaFavorita/home/MainActivity.kt
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.zup.minhaMusicaFavorita
+package br.com.zup.minhaMusicaFavorita.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.minhaMusicaFavorita.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
