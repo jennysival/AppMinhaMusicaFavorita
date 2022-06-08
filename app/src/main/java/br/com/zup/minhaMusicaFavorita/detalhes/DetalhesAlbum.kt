ece5e1d0ee8de3acea01123320d658e738d31672
jennysival <jeniffersival@gmail.com>
2022-06-08 11:25:28 -0300

    Implementar layout dos detalhes do album

--- app/src/main/java/br/com/zup/minhaMusicaFavorita/detalhes/DetalhesAlbum.kt
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/detalhes/DetalhesAlbum.kt
@@ -2,11 +2,23 @@ package br.com.zup.minhaMusicaFavorita.detalhes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import br.com.zup.minhaMusicaFavorita.R
 
 class DetalhesAlbum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes_album)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.detalhes_album)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
