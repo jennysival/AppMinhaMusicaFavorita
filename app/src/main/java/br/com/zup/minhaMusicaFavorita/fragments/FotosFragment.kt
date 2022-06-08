ead84fb577baafef3f21ff1950cdfed885b6551e
jennysival <jeniffersival@gmail.com>
2022-06-08 13:57:09 -0300

    Criar constantes para adicionar informações aos albuns da lista

--- app/src/main/java/br/com/zup/minhaMusicaFavorita/fragments/FotosFragment.kt
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/fragments/FotosFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.zup.minhaMusicaFavorita.R
+import br.com.zup.minhaMusicaFavorita.*
+import br.com.zup.minhaMusicaFavorita.databinding.FragmentFotosBinding
+import br.com.zup.minhaMusicaFavorita.model.Album
 
 class FotosFragment : Fragment() {
+    private lateinit var binding: FragmentFotosBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fotos, container, false)
+    ): View {
+        binding = FragmentFotosBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    private fun adicionarAlbumNaLista(){
+        val listaAlbuns = mutableListOf<Album>()
+
+        listaAlbuns.add(Album(R.drawable.album1, TITULO_ALBUM1, DESCRICAO_ALBUM1, LANCAMENTO_2009,
+            GRAVADORA_LAB_FANTASMA, ESTUDIOS_ALBUM1, FORMATO_CD_MIX, GENERO_HIPHOP))
     }
 }
