8402f8ffba9826296deb08c71c7fc57ce618ea1a
jennysival <jeniffersival@gmail.com>
2022-06-08 10:34:55 -0300

    Criar classe album e activity detalheAlbum

---
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/detalhes/DetalhesAlbum.kt
@@ -0,0 +1,12 @@
+package br.com.zup.minhaMusicaFavorita.detalhes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.minhaMusicaFavorita.R
+
+class DetalhesAlbum : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_album)
+    }
+}
---
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/model/Album.kt
@@ -0,0 +1,20 @@
+package br.com.zup.minhaMusicaFavorita.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+
+class Album(
+    private var titulo: String,
+    private var descricao: String,
+    private var lancamento: String,
+    private var gravadora: String,
+    private var estudio: String,
+    private var formato: String,
+    private var genero: String
+): Parcelable {
+
+    private val artista = "Emicida"
+
+}
