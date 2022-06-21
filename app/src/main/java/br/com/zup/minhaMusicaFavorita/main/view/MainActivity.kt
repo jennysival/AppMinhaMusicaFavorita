8aa90ff3848f38b2fd739c9865ef0777681cf6e4
jennysival <jeniffersival@gmail.com>
2022-06-20 22:21:25 -0300

    Adicionar viewModel

--- app/src/main/java/br/com/zup/minhaMusicaFavorita/main/view/MainActivity.kt
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/main/view/MainActivity.kt
@@ -3,11 +3,16 @@ package br.com.zup.minhaMusicaFavorita.main.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import br.com.zup.minhaMusicaFavorita.databinding.ActivityMainBinding
 import br.com.zup.minhaMusicaFavorita.detalhesViewPager.Detalhes
+import br.com.zup.minhaMusicaFavorita.main.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
