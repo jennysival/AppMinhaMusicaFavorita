00d49019ebe83b39f675aff529b51a90e20dcec5
jennysival <jeniffersival@gmail.com>
2022-06-20 22:18:58 -0300

    Adicionar funções get ao MainViewModel

--- app/src/main/java/br/com/zup/minhaMusicaFavorita/main/viewModel/MainViewModel.kt
+++ app/src/main/java/br/com/zup/minhaMusicaFavorita/main/viewModel/MainViewModel.kt
@@ -1,8 +1,56 @@
 package br.com.zup.minhaMusicaFavorita.main.viewModel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.minhaMusicaFavorita.main.repository.MainRepository
 
 class MainViewModel: ViewModel() {
     private val repository = MainRepository()
+
+    private val _responseImage: MutableLiveData<Int> = MutableLiveData()
+    val responseImage: LiveData<Int> = _responseImage
+
+    private val _responseTitle: MutableLiveData<String> = MutableLiveData()
+    val responseTitle: LiveData<String> = _responseTitle
+
+    private val _responseSubTitle: MutableLiveData<String> = MutableLiveData()
+    val responseSubTitle: LiveData<String> = _responseSubTitle
+
+    private val _responseLyrics: MutableLiveData<String> = MutableLiveData()
+    val responseLyrics: LiveData<String> = _responseLyrics
+
+    fun getImage(){
+        try {
+            _responseImage.value = repository.getImage()
+        }catch (ex: Exception){
+            Log.i("Error","-----> ${ex.message}")
+        }
+    }
+
+    fun getTitle(){
+        try {
+            _responseTitle.value = repository.getTitle()
+        }catch (ex: Exception){
+            Log.i("Error","-----> ${ex.message}")
+        }
+    }
+
+    fun getSubTitle(){
+        try {
+            _responseSubTitle.value = repository.getSubTitle()
+        }catch (ex: Exception){
+            Log.i("Error","-----> ${ex.message}")
+        }
+    }
+
+    fun getLyrics(){
+        try {
+            _responseLyrics.value = repository.getLyrics()
+        }catch (ex: Exception){
+            Log.i("Error","-----> ${ex.message}")
+        }
+    }
+
 }
